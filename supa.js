a6987b9c39df0784b657ae1fb13f8b8c37daf41e
saik12345 <saikatmukherjee.adm@gmail.com>
2025-07-25 01:16:58 +0530

    Initial commit

---
+++ supa.js
@@ -0,0 +1,94 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabaseUrl = "https://xzgrfpxfydnbmnxgqiki.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Z3JmcHhmeWRuYm1ueGdxaWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTA4MjYsImV4cCI6MjA2ODg2NjgyNn0.xScQIiye_zmIu5tefLNVWAJNVjX7pNBNF4wuxE-siAA";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+let options = [];
+
+//get-dokans
+async function getAllDokans() {
+  const { data: dokans, error } = await supabase.from("Dokan").select("name");
+  console.log(dokans);
+  dokans.map((dokan) => {
+    options.push(dokan.name);
+  });
+  console.log(options);
+
+  options.map((x) => {
+    const option = document.createElement("option");
+    option.textContent = x;
+    option.value = x;
+    dno?.appendChild(option);
+  });
+}
+getAllDokans();
+
+//add-dokans
+async function addDokan(e) {
+  e.preventDefault();
+
+  console.log(addDokanForm);
+  const dokanName = document.getElementById("new-dokan-name").value;
+  if (!dokanName) {
+    alert("Give a dokan name !!");
+    return;
+  }
+  const mob = document.getElementById("mob").value;
+  const eml = document.getElementById("eml").value;
+  const adr = document.getElementById("adr").value;
+  const { data, error } = await supabase
+    .from("Dokan")
+    .insert([{ name: dokanName, mobile: mob, email: eml, address: adr }])
+    .select();
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+}
+
+//calculate-due
+function calculateDue() {
+  let dueamt;
+  if (status.value == "given"){
+
+    dueamt = (amt.value * formula.value) / 99.5 - (92 * amt.value) / 99.5;
+    due.textContent = dueamt.toFixed(2);
+  }
+  else{
+    dueamt=
+  }
+}
+
+//index page
+const status = document.getElementById("status");
+const manualInput = document.getElementById("manual");
+const formula = document.getElementById("formula");
+const dno = document.getElementById("d-n-o");
+const due = document.getElementById("due");
+const amt = document.getElementById("amt");
+
+//event listeners
+status?.addEventListener("change", function () {
+  if (this.value == "received") {
+    manualInput.removeAttribute("hidden");
+    formula.setAttribute("hidden", true);
+  } else {
+    manualInput.setAttribute("hidden", true);
+    formula.removeAttribute("hidden");
+  }
+});
+amt?.addEventListener("input", calculateDue);
+formula?.addEventListener("change", calculateDue);
+status?.addEventListener("change", calculateDue);
+
+// for add dokan
+const addDokanForm = document.getElementById("dokan-add");
+addDokanForm?.addEventListener("submit", addDokan);
+const dokanName = document.getElementById("new-dokan-name");
+const mob = document.getElementById("mob");
+const eml = document.getElementById("eml");
+const adr = document.getElementById("adr");
+const addDokanButton = document.getElementById("add-dokan-button");
