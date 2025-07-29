8d0982036d5290ba0d61383c1b1ce846eafc8bdf
saik12345 <saikatmukherjee.adm@gmail.com>
2025-07-29 17:50:21 +0530

    logic

--- supa.js
+++ supa.js
@@ -333,7 +333,7 @@ async function filter({ dn, sd, ed, stat, transactionArea }) {
 if (
   window.location.href.includes("index") ||
   window.location.pathname == "/dokan/" ||
-  window.location.pathname == ""
+  window.location.pathname == "/"
 ) {
   const status = document.getElementById("status");
   const formula = document.getElementById("formula");
